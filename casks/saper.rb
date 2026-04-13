b2aeea40c8f281c286ba3922d3dcafb8ace9384e
Dmitry Stolyarov <42668398+dimark57@users.noreply.github.com>
2026-04-13 23:10:01 +0500

    Add cask for Saper game

---
+++ casks/saper.rb
@@ -0,0 +1,9 @@
+cask "saper" do
+  version "1.0.0"
+  // sha256 "...."
+  url "https://github.com/dimark57/saper/releases/download/v#{version}/Saper.app.zip"
+  name "Saper"
+  desc "Classic Minesweeper for macOS"
+  homepage "https://github.com/daimrk57/saper"
+  app "Saper.app"
+end
